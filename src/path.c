267807bdaacf9dac3dee00916ec1ab2b31ce7b2b
Scott Vokes <vokes.s@gmail.com>
2018-07-31 14:14:06 -0400

    If argument is a directory, list (template) files and exit.
    Closes #3.

--- src/path.c
+++ src/path.c
@@ -1,5 +1,11 @@
 #include "path.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+static void check_for_directory(const char *path);
+
 /* In order, check:
  *   ${NAME}
  *   ${SKEL_CLOSET:-~/.dem_bones}/${NAME}
@@ -15,11 +21,12 @@ FILE *path_open_skel_file(struct config *cfg, const char *name) {
     if (cfg->skel_path == NULL) { cfg->skel_path = DEF_HOME_PATH; }
 
     f = fopen(name, "r");
-    #define TRY_PATH(fmt, ...) \
+
+    #define TRY_PATH(fmt, ...)                                          \
         if (PATH_MAX <= snprintf(pathbuf, PATH_MAX, fmt, __VA_ARGS__)) {\
             return NULL;                                                \
         }                                                               \
-        if (0) { printf("trying path '%s'...\n", pathbuf); }            \
+        check_for_directory(pathbuf);                                   \
         f = fopen(pathbuf, "r")
 
     /* not found => try user's closet */
@@ -44,4 +51,28 @@ FILE *path_open_skel_file(struct config *cfg, const char *name) {
     errno = 0;                  /* suppress intemediate failures */
     return f;
     #undef TRY_PATH
+}
+
+static void check_for_directory(const char *path) {
+    struct stat st;
+    if (-1 == stat(path, &st)) {
+        if (errno == ENOENT) {
+            errno = 0;
+            return;
+        } else {
+            err(1, "stat: %s\n", path);
+        }
+    } else {
+        if (S_ISDIR(st.st_mode)) {
+            char cmd[] = "ls -1 ";
+            char cmd_buf[PATH_MAX + strlen(cmd)];
+            if (sizeof(cmd_buf) < (size_t)snprintf(cmd_buf, sizeof(cmd_buf),
+                    "%s%s", cmd, path)) {
+                exit(1);
+            }
+            fprintf(stderr, "%s is a directory, with:\n", path);
+            if (-1 == system(cmd_buf)) { err(1, "system"); }
+            exit(1);
+        }
+    }
 }
